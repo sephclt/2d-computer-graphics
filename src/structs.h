e2175a43f85ba2e3a6ddb335bf998e4851d727d5
sephclt <josephajero22@gmail.com>
2026-04-17 12:32:39 +0800

    feat: updated ImageTexture to have noise_value and a custom comparison operator

--- src/structs.h
+++ src/structs.h
@@ -14,4 +14,12 @@ struct Image {
 struct ImageTexture : Image {
     std::vector<double> noise;
     std::tuple<float, float, float> color;
+    float noise_value;
+
+    bool operator==(ImageTexture rhs) {
+        if (rhs.noise_value != this->noise_value)
+            return false;
+
+        return true;
+    }
 };
