7eb66fe53825d7ff1d52452bad6e664370de18c0
sephclt <josephajero22@gmail.com>
2026-04-20 00:27:47 +0800

    feat: added core_log

---
+++ core/src/core_log.cxx
@@ -0,0 +1,6 @@
+#include "core_log.h"
+#include <iostream>
+
+void core_log::print_message(const std::string message) {
+    std::cout << message << "\n";
+}
---
+++ core/src/core_log.h
@@ -0,0 +1,12 @@
+#ifndef CORE_LOG_H
+#define CORE_LOG_H
+
+#include <string>
+
+namespace core_log {
+
+void print_message(const std::string message);
+
+}
+
+#endif // !CORE_LOG_H
