7b8f2ade1a26c84b01aa3fdf0be1e5c8c01afcec
sephclt <josephajero22@gmail.com>
2026-04-20 00:27:42 +0800

    feat: added core_file

---
+++ core/src/core_file.cxx
@@ -0,0 +1,89 @@
+#include "core_file.h"
+#include "core_log.h"
+#include <fstream>
+
+void core_file::save_image(Image &image) {
+
+    std::tuple<float, float, float> color;
+    std::ofstream out(image.filename, std::ofstream::out);
+
+    core_log::print_message(std::format("Saving Image: {}", image.filename));
+
+    // Write P3
+    out << "P3\n" << image.width << " " << image.height << "\n255\n";
+
+    for (int i = 0; i < (image.width * image.height); ++i) {
+        color = image.texture[i];
+        out << int(std::get<0>(color) * 255.0f) << " "
+            << int(std::get<1>(color) * 255.0f) << " "
+            << int(std::get<2>(color) * 255.0f) << "\n";
+    }
+
+    core_log::print_message(
+        std::format("Done Saving Image: {}", image.filename));
+
+    out.close();
+}
+
+void core_file::read_image(Image &image) {
+
+    std::ifstream infile(image.filename);
+    std::tuple<int, int, int> r_color;
+    std::tuple<float, float, float> w_color;
+    std::tuple<int, int> width_and_height;
+
+    int state = 0;
+    bool done = false;
+    std::string line;
+
+    core_log::print_message(std::format("Reading Image: {}", image.filename));
+
+    while (std::getline(infile, line)) {
+        if (start_with_case_insensitive(line, "P3") && state == 0) {
+            state = 1;
+        }
+
+        if (!start_with_case_insensitive(line, "#") &&
+            !start_with_case_insensitive(line, "P3") && state == 1) {
+            width_and_height = get_width_and_height(line);
+            image.width = std::get<0>(width_and_height);
+            image.height = std::get<1>(width_and_height);
+            state = 2;
+        }
+
+        if (start_with_case_insensitive(line, "255") && state == 2) {
+            state = 3;
+        }
+
+        if (!(line.compare("255") == 0) && state == 3) {
+            r_color = get_color(line);
+            w_color = std::make_tuple(float(std::get<0>(r_color) / 255.0f),
+                                      float(std::get<1>(r_color) / 255.0f),
+                                      float(std::get<2>(r_color) / 255.0f));
+            image.texture.push_back(w_color);
+            done = true;
+        }
+    }
+
+    core_log::print_message(
+        std::format("Done Reading Image: {}", image.filename));
+
+    infile.close();
+}
+
+std::tuple<int, int, int> core_file::get_color(const std::string line) {
+    size_t start;
+    size_t end = 0;
+    std::vector<std::string> out;
+
+    while ((start = line.find_first_not_of(" ", end)) != std::string::npos) {
+        end = line.find(" ", start);
+        out.push_back(line.substr(start, end - start));
+    }
+
+    int r = std::stoi(out[0]);
+    int g = std::stoi(out[1]);
+    int b = std::stoi(out[2]);
+
+    return std::make_tuple(r, g, b);
+}
---
+++ core/src/core_file.h
@@ -0,0 +1,22 @@
+#ifndef CORE_FILE_H
+#define CORE_FILE_H
+
+#include "core.h"
+#include <string>
+#include <tuple>
+
+namespace core_file {
+
+bool start_with_case_insensitive(std::string line, std::string to_match);
+
+std::tuple<int, int> get_width_and_height(const std::string line);
+
+static std::tuple<int, int, int> get_color(const std::string line);
+
+void save_image(Image &image);
+
+void read_image(Image &image);
+
+} // namespace core_file
+
+#endif // CORE_FILE_H
