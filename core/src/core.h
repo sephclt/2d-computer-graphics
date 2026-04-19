4279158a7e22ddae952bbf8370904fc51795ce8d
sephclt <josephajero22@gmail.com>
2026-04-20 00:27:31 +0800

    feat: moved width and height properties from ImageFile to ImageTexture
    feat: added additional properties to ImageFile; filetype and dimensions

--- core/src/core.h
+++ core/src/core.h
@@ -9,14 +9,17 @@ enum SampleImageType : int;
 
 struct ImageFile {
     std::string filename;
-    int width;
-    int height;
+    std::string filetype;
+    std::string dimensions;
 };
 
 struct ImageTexture {
     std::vector<std::tuple<float, float, float>> texture;
     float noise_value;
     std::tuple<float, float, float> color;
+
+    int width;
+    int height;
 };
 
 struct Image : ImageFile, ImageTexture {};
